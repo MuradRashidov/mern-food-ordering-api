c33e47101b22a5782523b51caa0555935231b530
MuradRashidov <muradrashidov995@gmail.com>
2024-04-22 21:52:12 +0400

    manage-restaurant

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,19 @@ import express, { Request, Response } from "express";
 import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
-import myUserRoute from "./routes/MyUserRoute"
+import myUserRoute from "./routes/MyUserRoute";
+import { v2 as cloudinary } from "cloudinary";
+import myRestaurantRoute from "./routes/MyRestaurantRoute";
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string)
 .then(()=>{
     console.log("Connected to database");
     
+});
+const {CLOUDINARY_CLOUD_NAME, CLOUDINARY_API_KEY, CLOUDINARY_API_SECRET} = process.env;
+cloudinary.config({
+    cloud_name:CLOUDINARY_CLOUD_NAME,
+    api_key:CLOUDINARY_API_KEY,
+    api_secret:CLOUDINARY_API_SECRET
 })
 const app = express();
 app.use(express.json());
@@ -15,7 +23,7 @@ app.get("/health",(req:Request,res:Response)=>{
     res.send({message:"Health OK!"});
 })
 app.use("/api/my/user",myUserRoute);
-
+app.use("/api/my/restaurant",myRestaurantRoute)
 app.listen(7000,()=>{
     console.log("App is running localhost:7000");
 })
--- src/middleware/validation.ts
+++ src/middleware/validation.ts
@@ -3,6 +3,8 @@ import { body, validationResult } from "express-validator";
 const handleValidationErrors = (req:Request,res:Response,next:NextFunction) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
+        console.log(errors);
+        
         return res.status(400).json({errors:errors.array()})
     }
     next();
@@ -12,5 +14,18 @@ export const validateMyUserRequest = [
     body("addressLine1").isString().notEmpty().withMessage("AddressLine1 must be string"),
     body("city").isString().notEmpty().withMessage("City must be string"),
     body("country").isString().notEmpty().withMessage("Country must be string"),
+    handleValidationErrors
+]
+
+export const validateMyRestaurantRequest = [
+    body("restaurantName").isString().notEmpty().withMessage("Restaurant name must be string"),
+    body("city").notEmpty().withMessage("City is required"),
+    body("country").notEmpty().withMessage("Country is required"),
+    body("deliveryPrice").notEmpty().isFloat({min:0}).withMessage("Delivery Price must be a posetive number"),
+    body("estimatedDeliveryTime").isInt({min:0}).withMessage("Estimated Delivery Time must be a positive integer"),
+    body("cuisines").isArray().withMessage("Quisines must be an array").not().isEmpty().withMessage("Quisines must't be empty"),
+    body("menuItems").isArray().withMessage("Menu Items must't be empty"),
+    body("menuItems.*.name").notEmpty().withMessage("Menu Item Name must't be empty"),
+    body("menuItems.*.price").isFloat({min:1}).withMessage("Menu Item Price is required and must be positive number"),
     handleValidationErrors
 ]
